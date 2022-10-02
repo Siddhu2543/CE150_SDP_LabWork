b45bda2b2cc20a57691d2c4fc3259c4ff965c2db
Siddharth Vadgama <75413968+Siddhu2543@users.noreply.github.com>
2022-10-02 12:33:57 +0530

    Add files via upload

---
+++ Lab9-10/lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:lab_9_10/pages/choose_location.dart';
+import 'package:lab_9_10/pages/home.dart';
+import 'package:lab_9_10/pages/loading.dart';
+
+void main() => runApp(MaterialApp(
+  // home: Home(),
+  initialRoute: '/home',
+  routes: {
+    '/': (context) => Loading(),
+    '/home': (context) => Home(),
+    '/location': (context) => ChooseLocation(),
+  }
+));
+
+
